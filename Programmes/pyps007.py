02604bed172772404d8bbb7c0ac201e616d7335d
Static <asadhum2005@gmail.com>
2020-11-16 11:42:22 +0000

    + 2 tasks

---
+++ Programmes/pyps007.py
@@ -0,0 +1,7 @@
+'''
+Ask the user for their name and their age.  Add 1 to their age and display the output
+'''
+def fn(): 
+	name = str(input("What is your name? "))
+	age = int(input("What is your age? "))
+	print(str(age + 1))
--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from Programmes.pyps002 import fn as fn1
 from Programmes.pyps003 import fn as fn2
 from Programmes.pyps004 import fn as fn3
 from Programmes.pyps005 import fn as fn4
+from Programmes.pyps006 import fn as fn5
+from Programmes.pyps007 import fn as fn6
